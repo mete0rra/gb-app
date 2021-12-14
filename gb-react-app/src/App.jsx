85d4bb68976eaa49eeeddff9e74d8ed7834a49d5
mete0rra <noname.andrew.sarantsev@gmail.com>
2021-12-14 13:17:47 +0300

    task 1 done

---
+++ gb-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Message } from "./Message/Message";
+
+const name = "Andrew";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Message name={name} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
